199dc8e7c4be4d41fddbc09a36319a66e72cd55f
hoangminhduc <hoangminhduc.ite@gmail.com>
2025-03-07 15:21:09 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ca-central-1_HyBrdEmpy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '13ad039sejto9b930m2a5m9da0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4gfan958em49htt8ihjnl9hm8l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
